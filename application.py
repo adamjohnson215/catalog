2310b4299cc4346920aadf9a57464d9dd7e04f1d
Adam Johnson <adamjohnson215@gmail.com>
2015-08-21 10:24:40 -0400

    Update comments

--- application.py
+++ application.py
@@ -1,46 +1,46 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask (__name__)
 
-# routes for main and categories pages
+# Routes for main and categories pages
 @app.route('/')
 @app.route('/categories/')
-# Create 
+# Method for rendering all categories 
 def showCategories():
 	return 'This is the page that returns a list of categories!'
 
-# Create route for adding a new category
+# Route for adding a new category
 @app.route('/category/new/')
 def newCategory():
 	return 'This page will have a form to create a new category'
 
-# Create route for editing a category
+# Route for editing a category
 @app.route('/category/<int:category_id>/edit/')
 def editCategory(category_id):
 	return 'This page will have a form to edit category %s' % category_id
 
-# Create route for deleting a category
+# Route for deleting a category
 @app.route('/category/<int:category_id>/delete/')
 def deleteCategory(category_id):
 	return 'This page will have a form to delete category %s' % category_id
 
-# Create route to show a category's items
+# Routes for showing a category's items
 @app.route('/category/<int:category_id>/')
 @app.route('/category/<int:category_id>/items/')
 def showItem(category_id):
 	return 'This page will return all items in category %s' % category_id
 
-# Create route for adding a new item to a category
+# Route for adding a new item to a category
 @app.route('/category/<int:category_id>/item/new/')
 def newItem(category_id):
 	return 'This page will have a form to add a new item in category %s' % category_id
 
-# Create route for editing an item in a category
+# Route for editing an item in a category
 @app.route('/category/<int:category_id>/item/<int:item_id>/edit')
 def editItem(category_id, item_id):
 	return 'This page will have a form to edit item %s in category %s' %(item_id, category_id)
 
-# Create route for deleting an item in a category
+# Route for deleting an item in a category
 @app.route('/category/<int:category_id>/item/<int:item_id>/delete')
 def deleteItem(category_id, item_id):
 	return 'This page will have a form to delete item %s from category %s' %(item_id, category_id)
